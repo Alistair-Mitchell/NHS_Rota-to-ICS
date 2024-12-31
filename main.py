8a34abe5c74926a26973ea5688764c43b723a4a2
Alistair <ajmitchell1@sheffield.ac.uk>
2024-12-31 15:15:45 +0000

    updated minor comments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import tabula
 import numpy as np
 import re
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib
@@ -13,237 +13,221 @@ import pypdf
 import matplotlib as mpl
 mpl.rcParams['figure.dpi'] = 300
 import os
-os.environ["JAVA_HOME"] ="C:\Program Files\Java\jdk-19"
+os.environ["JAVA_HOME"] = "C:\Program Files\Java\jdk-19"
 
 # %matplotlib auto
 # %matplotlib inline
-file = "E:\Downloads\PdfAnalysis (2).pdf"
+file_path = "E:\Downloads\PdfAnalysis (2).pdf"
 
+pdf_dataframes = tabula.read_pdf(file_path, pages="2")
 
-dfs = tabula.read_pdf(file, pages="2")
+pdf_reader = pypdf.PdfReader(file_path)
+pdf_text = pdf_reader.pages[0].extract_text()
+doctor_name = pdf_text.splitlines()[2].split(", ")[0]
 
-reader = pypdf.PdfReader(file)
-text = reader.pages[0].extract_text()
-name = text.splitlines()[2].split(", ")[0]
+current_year = 2025
 
-year = 2025
-
-data = {'Upper bound': [100,  56,56,48,48,48,40],
-        'Lower bound': [56,48,48,40,40,40,   0],
-        'Multiplier': [2.0, 1.8,1.5,1.5,1.4, 1.2, 0.0]}
+banding_data = {
+    'Upper bound': [100, 56, 56, 48, 48, 48, 40],
+    'Lower bound': [56, 48, 48, 40, 40, 40, 0],
+    'Multiplier': [2.0, 1.8, 1.5, 1.5, 1.4, 1.2, 0.0]
+}
 
 # Creates pandas DataFrame.
-banding = pd.DataFrame(data, index=['3','2a','2b','1a','1b','1c','nb'])
+banding_table = pd.DataFrame(banding_data, index=['3', '2a', '2b', '1a', '1b', '1c', 'nb'])
 
+def calculate_duration(start_time, end_time):
+    """
+    Calculates the duration between two times.
+    """
+    time_format = '%H:%M'
+    time_delta = datetime.strptime(end_time, time_format) - datetime.strptime(start_time, time_format)
+    if time_delta.days < 0:
+        time_delta = timedelta(days=0, seconds=time_delta.seconds, microseconds=time_delta.microseconds)
+    return time_delta
 
-def dura(s1,s2):
-    FMT = '%H:%M'
-    tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
-    if tdelta.days < 0:
-        tdelta = timedelta(
-            days=0,
-            seconds=tdelta.seconds,
-            microseconds=tdelta.microseconds
-            )
-    return tdelta
-
-def convert_timedelta(duration):
+def convert_timedelta_to_hours(duration):
+    """
+    Converts a timedelta object into hours as a float value.
+    """
     days, seconds = duration.days, duration.seconds
-    # print(days, seconds)
     hours = days * 24 + seconds // 3600
     minutes = (seconds % 3600) // 60
-    seconds = (seconds % 60)
-    hours_2 = minutes/60
-    hours = hours+hours_2
-    return hours
+    return hours + minutes / 60
 
-def date_time_overlap(s1,e1,s2,e2):
+def calculate_overlap(start1, end1, start2, end2):
+    """
+    Calculates the overlap between two time ranges.
+    """
     Range = namedtuple('Range', ['start', 'end'])
-    r1 = Range(s1, e1)
-    # print("R1",r1)
-    r2 = Range(s2, e2)
-    # print("R2",r2)
-    latest_start = max(r1.start, r2.start)
-    earliest_end = min(r1.end, r2.end)
-    # earliest_end = min(r2.end, r2.end)
-    delta = (earliest_end - latest_start)
-    delta = convert_timedelta(delta)
-    # print("delta",delta)
-    overlap = max(0, delta)
-    # print(overlap)
-    # print(overlap)
-    return overlap
+    range1 = Range(start1, end1)
+    range2 = Range(start2, end2)
+    latest_start = max(range1.start, range2.start)
+    earliest_end = min(range1.end, range2.end)
+    overlap_duration = max(0, convert_timedelta_to_hours(earliest_end - latest_start))
+    return overlap_duration
 
-def get_rota(dfs):
-    basket = []    
-    i = 0
-    overlap = 0
-    antilap = 0
-    for pdf_sheet,extracted_frame  in enumerate(dfs) :        
-        for week_index, row in extracted_frame.iterrows():
-            week_frame = pd.DataFrame(row)
-            week_start = week_frame.values[0][0]
-            dt_week_start = datetime.strptime(week_start, '%d %b %Y')
-            for day_index, [day, shiftcell] in enumerate(week_frame[1:].iterrows()):
-                if shiftcell.isnull().any():
+def extract_rota(pdf_dataframes):
+    """
+    Extracts rota information from the provided PDF dataframes.
+    """
+    rota_entries = []    
+    weekend_count = 0
+    unsocial_hours_total = 0
+    overlap_hours_total = 0
+
+    for sheet_index, sheet in enumerate(pdf_dataframes):
+        for week_index, row in sheet.iterrows():
+            week_data = pd.DataFrame(row)
+            week_start_date = week_data.values[0][0]
+            dt_week_start = datetime.strptime(week_start_date, '%d %b %Y')
+
+            for day_index, (day, shift_details) in enumerate(week_data[1:].iterrows()):
+                if shift_details.isnull().any():
                     continue
-                shiftdate = dt_week_start+timedelta(day_index)
-                shiftmonth = shiftdate.month
-                g =shiftcell.iloc[0].split("\r")
 
-                shift = g[0]
-                
-                start_check_time = dt.time(7,0,0)
-                end_check_time = dt.time(19,0,0)
-                start_check_date=shiftdate.combine(
-                    shiftdate.replace(year=year).date(), 
-                    start_check_time
-                    )
-                end_check_date=shiftdate.combine(
-                    shiftdate.replace(year=year).date(), 
-                    end_check_time
-                    )
-                
+                shift_date = dt_week_start + timedelta(days=day_index)
+                shift_month = shift_date.month
+                shift_data = shift_details.iloc[0].split("\r")
+
+                shift_type = shift_data[0]
+                start_time = datetime.strptime(shift_date.strftime('%Y-%m-%d') + ' 07:00:00', '%Y-%m-%d %H:%M:%S')
+                end_time = datetime.strptime(shift_date.strftime('%Y-%m-%d') + ' 19:00:00', '%Y-%m-%d %H:%M:%S')
+
                 try:
-                    start,end =g[1].split()
-                    duration = dura(start,end)
-                    start_dt=shiftdate.combine(
-                        shiftdate.date(), 
-                        datetime.strptime(start, '%H:%M').time()
-                        )
-                    end_dt = start_dt+duration
-                
+                    start, end = shift_data[1].split()
+                    shift_duration = calculate_duration(start, end)
+                    shift_start = datetime.combine(shift_date.date(), datetime.strptime(start, '%H:%M').time())
+                    shift_end = shift_start + shift_duration
                 except:
-                    start_dt = shiftdate
-                    end_dt = shiftdate+ timedelta(days=1)
-                    duration = timedelta(0,0)
-                    
-                date = shiftdate.strftime('%a %d %b')
-                jam = [date,shift,start_dt,end_dt,duration,week_index+1,shiftmonth]
-                basket.append(jam)
-                # print(jam)
-                
-                uhoh=date_time_overlap(start_check_date,end_check_date,start_dt,end_dt)
-                dur_con = convert_timedelta(duration)
-                if dur_con ==0:
+                    shift_start = shift_date
+                    shift_end = shift_date + timedelta(days=1)
+                    shift_duration = timedelta(0, 0)
+
+                formatted_date = shift_date.strftime('%a %d %b')
+                rota_entry = [formatted_date, shift_type, shift_start, shift_end, shift_duration, week_index + 1, shift_month]
+                rota_entries.append(rota_entry)
+
+                overlap_hours = calculate_overlap(start_time, end_time, shift_start, shift_end)
+                total_shift_hours = convert_timedelta_to_hours(shift_duration)
+
+                if total_shift_hours == 0:
                     continue
-                # print(dur_con,uhoh)
-                # print(date,abs(uhoh-dur_con),"antisocial hours")
-                if (shiftdate.weekday() >4):
-                    # print(date)
-                    i+=1
-                    antilap = antilap+dur_con
-                antilap= antilap + abs(uhoh-dur_con)
-                overlap = overlap + uhoh
-                
-                
-        rota = pd.DataFrame(basket,columns=['hr_date', 'Shift','Start','End','Duration','Week','Month'])
-        k = rota['Start'].iloc[-1].date()-rota['Start'].iloc[0].date()
-        weeks = (k.days+1)//7
-        counts = rota['Shift'].value_counts()
-        hours = rota["Duration"].sum()
-            
-        hours=convert_timedelta(hours)
-        
-        
-        
-        try:
-            average_hours = round(hours/weeks,1)
-        except ZeroDivisionError:
-            result = 0
-        try:
-            night = counts["Night"]
-        except KeyError:
-            night = 0  
-            
-        
-        print("They work",hours, "total hours and does",night,"night shifts.")
-        print("They work an average of",average_hours,"hours per week over",weeks,"weeks")
-        print("They work",i,"weekend days, or",i//2,"weekends in",weeks,"weeks. This is an average of 1 in",round(weeks/(i//2),2))
-        print("They work a total of",antilap,"unsocial hours, which equates to",str(round(antilap/hours,4)*100)+"% of total","\n")
-        # band_checker2(hours,k.days+1,antilap)
-        summary = [hours,antilap,k.days+1,night,i,weeks]
-        return (summary,rota)
-    
-def rota_to_ics(rota,Doctor_Name):
-    wr = rota
-    cal = Calendar()
-    cal.add('prodid', '-//My calendar product//example.com//')
-    cal.add('version', '2.0')
-    for index,row in wr.iterrows():
+
+                if shift_date.weekday() > 4:
+                    weekend_count += 1
+                    unsocial_hours_total += total_shift_hours
+
+                unsocial_hours_total += abs(overlap_hours - total_shift_hours)
+                overlap_hours_total += overlap_hours
+
+    rota_df = pd.DataFrame(rota_entries, columns=['Date', 'Shift Type', 'Start Time', 'End Time', 'Duration', 'Week', 'Month'])
+    rota_period = rota_df['Start Time'].iloc[-1].date() - rota_df['Start Time'].iloc[0].date()
+    total_weeks = (rota_period.days +1)// 7 
+    shift_counts = rota_df['Shift Type'].value_counts()
+    total_hours = convert_timedelta_to_hours(rota_df['Duration'].sum())
+
+    try:
+        average_weekly_hours = round(total_hours / total_weeks, 1)
+    except ZeroDivisionError:
+        average_weekly_hours = 0
+
+    try:
+        night_shifts = shift_counts.get("Night", 0)
+    except KeyError:
+        night_shifts = 0
+
+    print("Total hours:", total_hours)
+    print("Night shifts:", night_shifts)
+    print("Average weekly hours:", average_weekly_hours)
+    print("Weekend days:", weekend_count, "Weekends:", weekend_count // 2)
+    print("Unsocial hours:", unsocial_hours_total, f"({round(unsocial_hours_total / total_hours * 100, 2)}% of total)")
+
+    summary = [total_hours, unsocial_hours_total, rota_period.days +1, night_shifts, weekend_count]
+    return summary, rota_df
+
+def export_rota_to_ics(rota, doctor_name):
+    """
+    Exports the rota information to an ICS calendar file.
+    """
+    calendar = Calendar()
+    calendar.add('prodid', '-//My calendar product//example.com//')
+    calendar.add('version', '2.0')
+
+    for _, row in rota.iterrows():
         event = Event()
-        event.add('summary', row['Shift'])
-        event.add('dtstart', row['Start'])
-        event.add('dtend', row['End'])
+        event.add('summary', row['Shift Type'])
+        event.add('dtstart', row['Start Time'])
+        event.add('dtend', row['End Time'])
         event['location'] = vText('Raigmore Hospital,Old Perth Rd, Inverness IV2 3UJ')
-        
-        cal.add_component(event)
+        calendar.add_component(event)
 
-    name = f"{Doctor_Name} Rota, {wr['hr_date'].iloc[0]} - {wr['hr_date'].iloc[-1]}.ics"
+    file_name = f"{doctor_name} Rota, {rota['Date'].iloc[0]} - {rota['Date'].iloc[-1]}.ics"
 
-    f = open(name, 'wb')
-    f.write(cal.to_ical())
-    f.close()
-    # print("\n")
-    print(name,"exported")
-    return wr
-    
-def band_checker(wr):
-    for week  in range(wr['Week'].iloc[-1]):
-        working_week =  wr.loc[wr["Week"]==week]
-        # print(working_week)
-        
-        # jam = [date,shift,start_dt,end_dt,duration,week_sheet+1]
-        # basket.append(jam)
-    # summary = pd.DataFrame(basket,columns=['Total hours', 'Unsocial hours','Weekend work?','Unsocial proportion','weeks','Week'])
+    with open(file_name, 'wb') as file:
+        file.write(calendar.to_ical())
+
+    print(file_name, "exported")
+    return rota
+
+def band_checker(summary):
+    """
+    Placeholder for band checking function logic.
+    """
+    pass
+
 def band_checker2(summary):
-    hours,unsocial,days,nights,weekends,weeks= summary  
-    hpd = hours/days
+    """
+    Estimates the band based on summary data.
+    """
+    total_hours, unsocial_hours, total_days, night_shifts, weekend_days = summary  
+    hours_per_day = total_hours / total_days
     band = ""
+
     while True:
-        user_input = input("\nDo you work an on call rota: ")
+        user_input = input("\nDo you work an on-call rota? (yes/no): ")
         if user_input.lower() in ["yes", "y"]:
-            OC = True
+            is_on_call = True
             break
         elif user_input.lower() in ["no", "n"]:
-            OC = False
+            is_on_call = False
             break
         else:
-            print("Invalid input. Please enter yes/no.")
-    print(hpd)
-    match hpd:
-        case a if 56/7 <= a <  100/7:
-            band = "3"  
-        case b if 48/7 <= b <  56/7:
+            print("Invalid input. Please enter yes or no.")
+    
+    match hours_per_day:
+        
+        case h if 56 / 7 <= h < 100 / 7:
 
-            match OC:
-                case True:
-                    band = "2a or 2b"
-                case False:
-                    band = "2a or 2b"
-        case v if 40/7 <= v <  48/7:
-            match (unsocial/hours)>(1/3):
-                case True:
-                    band = "1a"
-                case False:
-                    band = "1b"
-        case d if 0/7 <= d <  40/7:
+            band = "3"
+        case h if 48 / 7 <= h < 56 / 7:
+
+            band = "2a" if is_on_call else "2b"
+        case h if 40 / 7 <= h < 48 / 7:
+
+            band = "1a" if (unsocial_hours / total_hours) > (1 / 3) else "1b"
+        case h if 0 / 7 <= h < 40 / 7:
             band = "nb"
         case _:
-            print("uhoh")
+            print("Error determining band.")
+
     print("\nEstimated band:")
-    print(banding.loc[band].to_markdown(),"\n(Note, band 1c not accounted for)")
+    print(banding_table.loc[band].to_markdown(), "\n(Note, band 1c not accounted for)")
+    
+    
     
 
-def hours_per_month(wr):
-    for x in wr["Month"].unique():
-        
-        egg = wr.loc[wr["Month"]==x]
-        egg2 = convert_timedelta(egg['Duration'].sum(axis=0))
-        print("Month:",str(x)+",","Hours worked:",egg2)
+def hours_per_month(rota):
+    """
+    Calculates and prints the total hours worked for each month.
+    """
+    for month in rota["Month"].unique():
+        month_data = rota.loc[rota["Month"] == month]
+        total_hours = convert_timedelta_to_hours(month_data['Duration'].sum())
+        print("Month:", str(month) + ",", "Hours worked:", total_hours)
 
-summary_data,rota=get_rota(dfs)
-chosen_rota = rota_to_ics(rota,name)
+summary_data, rota = extract_rota(pdf_dataframes)
+chosen_rota = export_rota_to_ics(rota, doctor_name)
 band_checker2(summary_data)
 
 # Band 3	For those working more than 56 hours per week on average or not achieving the required rest. Non-compliant with the new deal, because of excessive hours or other matters.	100%
